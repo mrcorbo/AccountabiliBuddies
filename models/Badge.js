4236f0c065817d75a61bfefe65372800c345f489
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2022-11-07 21:13:45 -0700

    Updated eslint and started badge model & index.js

--- models/Badge.js
+++ models/Badge.js
@@ -3,5 +3,19 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class Badge extends Model {
-    
+
 }
+Badge.init(
+    {
+        id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        },
+        badge_id: {
+
+        }   
+    }
+)
+
