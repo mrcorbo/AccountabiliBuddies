d0e916dd75970729e94d29f8fe0097bbd62a5d6c
ReTims <renatatims@gmail.com>
2022-11-07 22:21:45 -0600

    router.get to login page

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,2 +1,10 @@
 const router = require('express').Router();
 const { User, Goal, Badge } = require('../models');
+
+router.get('/login', async (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+      }
+    res.render("login")
+});
