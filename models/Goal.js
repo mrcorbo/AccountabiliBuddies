03c1d20a567aba43bc27e1067c3f4b96f5da8f48
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2022-11-07 21:54:44 -0700

    Updated Goal model

--- models/Goal.js
+++ models/Goal.js
@@ -1,3 +1,29 @@
-// name
-// frequency
-// duration
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Goal extends Model {
+
+}
+Goal.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        weeklyFreq: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull:true
+        }
+    }
+);
