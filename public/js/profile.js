e9f5bc0643684105e66ccec01e425767fe8b78fd
ReTims <renatatims@gmail.com>
2022-11-09 16:04:32 -0600

    Added js logic to create a new Accountability on the user's profile page - function newGOal created

---
+++ public/js/profile.js
@@ -0,0 +1,29 @@
+// Creates a new goal in the user's profile page
+const newGoal = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#newGoal').value.trim();
+    const frequency = document.querySelector('#frequency').value.trim();
+    const duration = document.querySelector('#duration').value.trim();
+
+  
+    if (name && frequency && duration) {
+      const response = await fetch(`/api/goals`, {
+        method: 'goal',
+        body: JSON.stringify({ name, frequency, duration }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create goal');
+      }
+    }
+  };
+
+  document
+  .querySelector('#new-goal-form')
+  .addEventListener('submit', newGoal);
