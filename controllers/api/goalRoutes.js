aca8954549593c32104b4c32451574d6d39dc7aa
ReTims <renatatims@gmail.com>
2022-11-10 14:22:06 -0600

    Added delete goal logic to profile.js

--- controllers/api/goalRoutes.js
+++ controllers/api/goalRoutes.js
@@ -31,7 +31,7 @@ router.delete("/:id", withAuth, async (req, res) => {
       return;
     }
 
-    res.status(200).json(postData);
+    res.status(200).json(goalData);
   } catch (err) {
     res.status(500).json(err);
   }
