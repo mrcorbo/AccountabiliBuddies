51533faee6f3c754f110ce408daa99837b91f8a7
ReTims <renatatims@gmail.com>
2022-11-10 14:04:27 -0600

    Added router.delete to goalRoutes.js

--- controllers/api/goalRoutes.js
+++ controllers/api/goalRoutes.js
@@ -1,19 +1,40 @@
-const router = require('express').Router();
-const { Goal } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Goal } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // Creates new goal
-router.post('/', withAuth, async (req, res) => {
-    try {
-      const createNewGoal = await Goal.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(createNewGoal);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const createNewGoal = await Goal.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-  module.exports = router;
+    res.status(200).json(createNewGoal);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Deletes Goal by ID
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const goalData = await Goal.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!goalData) {
+      res.status(404).json({ message: "No goal found with this id!" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
