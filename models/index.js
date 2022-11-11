c2a823ffc2ae588cfdadfa8808f173296d70492b
ReTims <renatatims@gmail.com>
2022-11-11 11:05:38 -0600

    Created new model for Message + included message relations on index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const User = require('./User');
 const Goal = require('./Goal');
 const Badge = require('./Badge');
+const Message = require('./Badge');
 
 // Badge belongsToMany User
 Badge.belongsTo(User, {
@@ -21,8 +22,21 @@ Goal.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+//Message relations
+
+User.hasMany(Message, {
+  foreignKey: 'user_id'
+
+});
+
+Message.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
 module.exports = {
   Goal,
   Badge,
-  User
+  User,
+  Message
 };
